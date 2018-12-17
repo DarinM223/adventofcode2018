b29dcf105ef16f93d3657b47116b9e4366d32651
Darin Minamoto <darinm223@gmail.com>
2018-12-17 06:07:46 -0800

    Refactors day 17 to only use one grid

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,9 +1,6 @@
 module Day17 where
 
-import Control.Applicative (liftA2)
-import Control.Monad.Reader
-import Control.Monad.State
-import Data.Array
+import Control.Monad.State.Strict
 import Data.Foldable
 import Data.Maybe (fromJust)
 import Data.Void
@@ -16,8 +13,12 @@ data Coord = Fixed Int | Range Int Int
   deriving (Show, Eq)
 
 type Point = (Coord, Coord) -- (y, x)
-type Grid = Array (Int, Int) Char
-type Waters = HM.HashMap (Int, Int) Char
+type GridMap = HM.HashMap (Int, Int) Char
+data Grid = Grid
+  { _minBounds :: (Int, Int)
+  , _maxBounds :: (Int, Int)
+  , _grid      :: GridMap
+  } deriving (Show, Eq)
 
 parseLine :: String -> Point
 parseLine s = case runParser parse "" s of
@@ -36,76 +37,58 @@ parseLine s = case runParser parse "" s of
   parseCoord = try (Range <$> L.decimal <*> (string ".." *> L.decimal))
            <|> (Fixed <$> L.decimal)
 
-newtype GridT m a = GridT { unGridT :: ReaderT Grid (StateT Waters m) a }
-  deriving (Functor, Applicative, Monad, MonadIO, MonadReader Grid, MonadState Waters)
+newtype GridT m a = GridT { unGridT :: StateT Grid m a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadState Grid)
 
-runGridT :: Grid -> Waters -> GridT m a -> m (a, Waters)
-runGridT grid waters = flip runStateT waters . flip runReaderT grid . unGridT
+runGridT :: Grid -> GridT m a -> m (a, Grid)
+runGridT grid = flip runStateT grid . unGridT
 
-getCell :: (MonadState Waters m, MonadReader Grid m)
-        => (Int, Int) -> m (Maybe Char)
-getCell p = do
-  map <- get
-  case HM.lookup p map of
-    Just w  -> return $ Just w
-    Nothing -> do
-      grid <- ask
-      let bnds = bounds grid
-      if inRange bnds p
-        then return $ Just $ grid ! p
-        else return Nothing
+getCell :: MonadState Grid m => (Int, Int) -> m (Maybe Char)
+getCell p = HM.lookup p <$> gets _grid
 
-setCell :: (MonadState Waters m) => (Int, Int) -> Char -> m ()
-setCell p ch = modify' (HM.insert p ch)
+setCell :: (MonadState Grid m) => (Int, Int) -> Char -> m ()
+setCell p ch = modify' (\grid -> grid { _grid = HM.insert p ch (_grid grid) })
 
 mkGrid :: [Point] -> Grid
-mkGrid points = initarr // changes
+mkGrid points = Grid
+  { _minBounds = (miny, minx')
+  , _maxBounds = (maxy, maxx')
+  , _grid      = foldl' (\grid (k, v) -> HM.insert k v grid) init changes
+  }
  where
   (miny, maxy, minx, maxx, changes) = foldl'
     buildGrid
-    (maxBound :: Int, minBound :: Int, maxBound :: Int, minBound :: Int, [])
+    (maxBound, minBound, maxBound, minBound, [])
     points
-  (minx', maxx') = (minx - 100, maxx + 100)
-  initarr = array ((miny, minx'), (maxy, maxx')) $ do
-    y <- [miny..maxy]
-    x <- [minx'..maxx']
-    return ((y, x), '.')
+  (minx', maxx') = (minx - 10, maxx + 10)
+  init = HM.fromList [((y, x), '.') | y <- [miny..maxy], x <- [minx'..maxx']]
   xrange y x1 x2 = [(y, x) | x <- [x1..x2]]
   yrange x y1 y2 = [(y, x) | y <- [y1..y2]]
   buildGrid (!miny, !maxy, !minx, !maxx, l) = \case
     (Fixed y, Range x1 x2) ->
-      ( min miny y
-      , max maxy y
-      , minimum [minx, x1, x2]
-      , maximum [maxx, x1, x2]
-      , l ++ zip (xrange y x1 x2) (repeat '#')
-      )
+      ( min miny y, max maxy y, minimum [minx, x1, x2], maximum [maxx, x1, x2]
+      , l ++ zip (xrange y x1 x2) (repeat '#') )
     (Range y1 y2, Fixed x) ->
-      ( minimum [miny, y1, y2]
-      , maximum [maxy, y1, y2]
-      , min minx x
-      , max maxx x
-      , l ++ zip (yrange x y1 y2) (repeat '#')
-      )
+      ( minimum [miny, y1, y2], maximum [maxy, y1, y2], min minx x, max maxx x
+      , l ++ zip (yrange x y1 y2) (repeat '#') )
     _ -> error "Both dimensions are ranges!"
 
 fillWater :: Monad m => (Int, Int) -> GridT m Char
-fillWater p@(y, x) = do
-  ch <- getCell p
-  case ch of
-    Just '#' -> return '#'
-    Nothing  -> return '|'
-    _        -> fillWater (y + 1, x) >>= \case
-      '|' -> '|' <$ setCell (y, x) '|'
-      '#' -> fillRow (y, x)
-      '~' -> fillRow (y, x)
-      c   -> error $ "Invalid char: " ++ show c
+fillWater p@(y, x) = getCell p >>= \case
+  Just '#' -> return '#'
+  Nothing  -> return '|'
+  _        -> fillWater (y + 1, x) >>= \case
+    '|' -> '|' <$ setCell (y, x) '|'
+    '#' -> fillRow (y, x)
+    '~' -> fillRow (y, x)
+    c   -> error $ "Invalid char: " ++ show c
 
 fillRow :: Monad m => (Int, Int) -> GridT m Char
 fillRow p@(y, _) = do
-  leftBound@(_, lx) <- scanRow (subtract 1) (+ 1) p
-  rightBound@(_, rx) <- scanRow (+ 1) (subtract 1) p
-  liftA2 (,) (getCell leftBound) (getCell rightBound) >>= \case
+  grid <- gets _grid
+  let leftBound@(_, lx)  = scanRow grid (subtract 1) p
+      rightBound@(_, rx) = scanRow grid (+ 1) p
+  case (HM.lookup leftBound grid, HM.lookup rightBound grid) of
     (Just '#', Just '#') -> do
       mapM_ (\x -> setCell (y, x) '~') [lx + 1..rx - 1]
       return '~'
@@ -118,55 +101,47 @@ fillRow p@(y, _) = do
       fromJust <$> getCell p
     _ -> error "This should not happen"
 
-scanRow :: Monad m
-        => (Int -> Int) -> (Int -> Int) -> (Int, Int) -> GridT m (Int, Int)
-scanRow f b (y, x) = liftA2 (,) (getCell (y, x)) (getCell (y + 1, x)) >>= \case
-  (Just '#', _)        -> return (y, x)
-  (Just '.', Just '.') -> return (y, x)
-  (Just '|', Just '|') -> return (y, x)
-  (Just _, _)          -> scanRow f b (y, f x)
-  (Nothing, _)         -> return (y, b x)
+scanRow :: GridMap -> (Int -> Int) -> (Int, Int) -> (Int, Int)
+scanRow grid f (y, x) =
+  case (HM.lookup (y, x) grid, HM.lookup (y + 1, x) grid) of
+    (Just '#', _)        -> (y, x)
+    (Just '.', Just '.') -> (y, x)
+    (Just '|', Just '|') -> (y, x)
+    (Just _, _)          -> scanRow grid f (y, f x)
+    (Nothing, _)         -> error "Grid X range too small"
 
-fillParams :: (Char, (Int, Int), (Int, Int))
-fillParams = undefined
-
-parseFile :: FilePath -> IO [Point]
-parseFile path = do
-  ls <- lines <$> readFile path
-  return $ fmap parseLine ls
-
-printGrid :: (MonadIO m, MonadReader Grid m, MonadState Waters m) => m ()
+printGrid :: (MonadIO m, MonadState Grid m) => m ()
 printGrid = do
-  ((miny, minx), (maxy, maxx)) <- bounds <$> ask
+  (miny, minx) <- gets _minBounds
+  (maxy, maxx) <- gets _maxBounds
   forM_ [miny..maxy] $ \y -> do
     forM_ [minx..maxx] $ \x -> getCell (y, x) >>= liftIO . putChar . fromJust
     liftIO $ putChar '\n'
 
-countWater :: (Monad m) => (Char -> Bool) -> GridT m Int
+countWater :: MonadState Grid m => (Char -> Bool) -> m Int
 countWater fn = fmap snd $ flip runStateT 0 $ do
-  ((miny, minx), (maxy, maxx)) <- bounds <$> lift ask
-  forM_ [miny..maxy] $ \y ->
-    forM_ [minx..maxx] $ \x -> lift (getCell (y, x)) >>= \case
-      Just c | fn c -> modify' (+ 1)
-      _             -> return ()
+  (miny, minx) <- lift $ gets _minBounds
+  (maxy, maxx) <- lift $ gets _maxBounds
+  forM_ [(y, x) | y <- [miny..maxy], x <- [minx..maxx]] $ \p ->
+    lift (getCell p) >>= maybe (pure ()) (\c -> when (fn c) (modify' (+ 1)))
+
+parseFile :: FilePath -> IO [Point]
+parseFile path = fmap parseLine . lines <$> readFile path
 
 day17part1 :: FilePath -> IO ()
 day17part1 path = do
   points <- parseFile path
-  let grid           = mkGrid points
-      ((miny, _), _) = bounds grid
-      fn c           = c == '|' || c == '~'
-  (water, _) <- runGridT grid HM.empty $
+  let grid      = mkGrid points
+      (miny, _) = _minBounds grid
+      fn c      = c == '|' || c == '~'
+  (water, _) <- runGridT grid $
     fillWater (miny, 500) *> countWater fn <* printGrid
-  print $ bounds grid
   print water
 
 day17part2 :: FilePath -> IO ()
 day17part2 path = do
   points <- parseFile path
-  let grid           = mkGrid points
-      ((miny, _), _) = bounds grid
-  (water, _) <- runGridT grid HM.empty $
-    fillWater (miny, 500) *> countWater (== '~') <* printGrid
-  print $ bounds grid
+  let grid      = mkGrid points
+      (miny, _) = _minBounds grid
+  (water, _) <- runGridT grid $ fillWater (miny, 500) *> countWater (== '~')
   print water
